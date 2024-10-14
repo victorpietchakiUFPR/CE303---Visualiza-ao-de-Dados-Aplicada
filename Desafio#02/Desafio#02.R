43f70944b4b83f8a82ac56d9b9cad021986507fc
Joao Victor Pietchaki Gonçalves <eizo.sakai@ufpr.br>
2024-10-14 17:15:33 -0300

    Atualizando arquivos. Fase final.

--- Desafio#02/Desafio#02.R
+++
@@ -1,8 +0,0 @@
-install.packages("data.table")
-library("data.table")
-
-dados <- fread("C:\\Users\\joaov\\Downloads\\Desafio#2-CE303-Visualizacao-de-Dados-Aplicada\\user_data.csv")
-head(dados, 5)
-
-dados_agregados <- dados[, .(stage_count = .N), by = stage]
-dados_agregados[, 1]
